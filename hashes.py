29c8a69d0f244ee9e0a81bd62d26472d0c3a1d7f
Matthias <70231371+matthiasnm@users.noreply.github.com>
2023-02-25 23:46:53 -0700

    Add files via upload
    Just wrote something to compare the user inputted file hashes.

---
+++ hashes.py
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Comparing the hashes of two files.
+
+a = ()
+
+b = () 
+
+a = input('Enter the first Hash of file1: ')
+
+b = input('Enter the second Hash of file2: ')
+
+if a == b:
+    print ('The hashes are the same')
+
+else: 
+    print('The hashes are not the same check that the hashes were entered correctly. ')
+
+
+
